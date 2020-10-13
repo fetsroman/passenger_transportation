35149c677b2cd0ddce8ebf5524249cfb86894664
fetsroman <fets.roman@gmail.com>
2020-10-13 18:10:14 +0300

    Add toggle and fixes

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-    $('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
-    });
-})
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+document.getElementById("anim-call-btn").onclick = function(){
+    mylogo.classList.toggle('fade');
+}
+
+$('#anim-call-btn').on('click', function() {
+    $(".phone1").toggle("show")
+    $(".phone2").toggle("show")
+});
